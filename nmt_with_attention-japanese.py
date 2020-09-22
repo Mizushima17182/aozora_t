4a963efb4a5b2b55ef7a10edc81ad88ef8df43c7
mizushima <51814668+Mizushima17182@users.noreply.github.com>
2020-09-22 21:01:22 +0900

    Add files via upload

---
+++ nmt_with_attention-japanese.py
@@ -0,0 +1,421 @@
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+
+# 日本語フォントをダウンロードする。
+!apt-get -y install fonts-ipafont-gothic
+
+# キャッシュを削除する。
+# !rm /root/.cache/matplotlib/fontList.json　# 旧cache
+!rm /root/.cache/matplotlib/fontlist-v300.json # 消すべきcache
+
+# ランタイムを再起動する。
+
+!apt install aptitude
+!aptitude install mecab libmecab-dev mecab-ipadic-utf8 git make curl xz-utils file -y
+!pip install mecab-python3==0.7
+
+# ローカルファイルを直接アップロードする
+from google.colab import files
+uploaded = files.upload()
+
+!unzip UniDic-kindai_1603.zip
+
+import MeCab
+
+# 対訳ファイルのダウンロード
+# ローカルファイルを直接アップロードする
+from google.colab import files
+uploaded = files.upload()
+
+ 
+
+ls
+
+
+path_to_file = "./jpn.txt"
+
+
+# ユニコードファイルを ascii に変換
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+
+    # 単語とそのあとの句読点の間にスペースを挿入
+    # 例：　"he is a boy." => "he is a boy ."
+    # 参照：- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+
+    # (a-z, A-Z, ".", "?", "!", ",") 以外の全ての文字をスペースに置き換え
+    #w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.rstrip().strip()
+
+    # 文の開始と終了のトークンを付加
+    # モデルが予測をいつ開始し、いつ終了すれば良いかを知らせるため
+    tagger = MeCab.Tagger("-Owakati -d UniDic-kindai_1603")
+    w = '<start> ' + tagger.parse(w) + ' <end>'
+    return w
+
+en_sentence = u"May I borrow this book?"
+sp_sentence = u"¿Puedo tomar prestado este libro?"
+print(preprocess_sentence(en_sentence))
+print(preprocess_sentence(sp_sentence).encode('utf-8'))
+
+# 1. アクセント記号を除去
+# 2. 文をクリーニング
+# 3. [ENGLISH, SPANISH] の形で単語のペアを返す
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+#    for i in lines[5]:
+      #print(i)
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
+
+    return zip(*word_pairs)
+
+en, sp = create_dataset(path_to_file, None)
+print(en[:10])
+print(sp[:10])
+
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+def tokenize(lang):
+  lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+      filters='')
+  lang_tokenizer.fit_on_texts(lang)
+
+  tensor = lang_tokenizer.texts_to_sequences(lang)
+
+  tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+                                                         padding='post')
+
+  return tensor, lang_tokenizer
+
+def load_dataset(path, num_examples=None):
+    # クリーニングされた入力と出力のペアを生成
+    targ_lang, inp_lang = create_dataset(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+# このサイズのデータセットで実験
+num_examples = 3000
+input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(path_to_file, num_examples)
+
+# ターゲットテンソルの最大長を計算
+max_length_targ, max_length_inp = max_length(target_tensor), max_length(input_tensor)
+
+# 80-20で分割を行い、訓練用と検証用のデータセットを作成
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+# 長さを表示
+print(len(input_tensor_train), len(target_tensor_train), len(input_tensor_val), len(target_tensor_val))
+
+def convert(lang, tensor):
+  for t in tensor:
+    if t!=0:
+      print ("%d ----> %s" % (t, lang.index_word[t]))
+
+print ("Input Language; index to word mapping")
+convert(inp_lang, input_tensor_train[0])
+print ()
+print ("Target Language; index to word mapping")
+convert(targ_lang, target_tensor_train[0])
+
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 64
+steps_per_epoch = len(input_tensor_train)//BATCH_SIZE
+embedding_dim = 256
+units = 1024
+vocab_inp_size = len(inp_lang.word_index)+1
+vocab_tar_size = len(targ_lang.word_index)+1
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+example_input_batch, example_target_batch = next(iter(dataset))
+example_input_batch.shape, example_target_batch.shape
+
+class Encoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    super(Encoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.enc_units = enc_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+  def call(self, x, hidden):
+    x = self.embedding(x)
+    output, state = self.gru(x, initial_state = hidden)
+    return output, state
+
+  def initialize_hidden_state(self):
+    return tf.zeros((self.batch_sz, self.enc_units))
+
+encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+
+# サンプル入力
+sample_hidden = encoder.initialize_hidden_state()
+sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
+print ('Encoder output shape: (batch size, sequence length, units) {}'.format(sample_output.shape))
+print ('Encoder Hidden state shape: (batch size, units) {}'.format(sample_hidden.shape))
+
+class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, query, values):
+    # hidden shape == (batch_size, hidden size)
+    # hidden_with_time_axis shape == (batch_size, 1, hidden size)
+    # スコアを計算するためにこのように加算を実行する
+    hidden_with_time_axis = tf.expand_dims(query, 1)
+
+    # score shape == (batch_size, max_length, 1)
+    # スコアを self.V に適用するために最後の軸は 1 となる
+    # self.V に適用する前のテンソルの shape は  (batch_size, max_length, units)
+    score = self.V(tf.nn.tanh(
+        self.W1(values) + self.W2(hidden_with_time_axis)))
+
+    # attention_weights の shape == (batch_size, max_length, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector の合計後の shape == (batch_size, hidden_size)
+    context_vector = attention_weights * values
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+
+attention_layer = BahdanauAttention(10)
+attention_result, attention_weights = attention_layer(sample_hidden, sample_output)
+
+print("Attention result shape: (batch size, units) {}".format(attention_result.shape))
+print("Attention weights shape: (batch_size, sequence_length, 1) {}".format(attention_weights.shape))
+
+class Decoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+    super(Decoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.dec_units = dec_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.fc = tf.keras.layers.Dense(vocab_size)
+
+    # アテンションのため
+    self.attention = BahdanauAttention(self.dec_units)
+
+  def call(self, x, hidden, enc_output):
+    # enc_output の shape == (batch_size, max_length, hidden_size)
+    context_vector, attention_weights = self.attention(hidden, enc_output)
+
+    # 埋め込み層を通過したあとの x の shape  == (batch_size, 1, embedding_dim)
+    x = self.embedding(x)
+
+    # 結合後の x の shape == (batch_size, 1, embedding_dim + hidden_size)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+    # 結合したベクトルを GRU 層に渡す
+    output, state = self.gru(x)
+
+    # output shape == (batch_size * 1, hidden_size)
+    output = tf.reshape(output, (-1, output.shape[2]))
+
+    # output shape == (batch_size, vocab)
+    x = self.fc(output)
+
+    return x, state, attention_weights
+
+decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE)
+
+sample_decoder_output, _, _ = decoder(tf.random.uniform((64, 1)),
+                                      sample_hidden, sample_output)
+
+print ('Decoder output shape: (batch_size, vocab size) {}'.format(sample_decoder_output.shape))
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+  loss_ = loss_object(real, pred)
+
+  mask = tf.cast(mask, dtype=loss_.dtype)
+  loss_ *= mask
+
+  return tf.reduce_mean(loss_)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+                                 encoder=encoder,
+                                 decoder=decoder)
+
+@tf.function
+def train_step(inp, targ, enc_hidden):
+  loss = 0
+
+  with tf.GradientTape() as tape:
+    enc_output, enc_hidden = encoder(inp, enc_hidden)
+
+    dec_hidden = enc_hidden
+
+    dec_input = tf.expand_dims([targ_lang.word_index['<start>']] * BATCH_SIZE, 1)
+
+    # Teacher Forcing - 正解値を次の入力として供給
+    for t in range(1, targ.shape[1]):
+      # passing enc_output to the decoder
+      predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
+
+      loss += loss_function(targ[:, t], predictions)
+
+      # Teacher Forcing を使用
+      dec_input = tf.expand_dims(targ[:, t], 1)
+
+  batch_loss = (loss / int(targ.shape[1]))
+
+  variables = encoder.trainable_variables + decoder.trainable_variables
+
+  gradients = tape.gradient(loss, variables)
+
+  optimizer.apply_gradients(zip(gradients, variables))
+
+  return batch_loss
+
+EPOCHS = 10
+
+for epoch in range(EPOCHS):
+  start = time.time()
+
+  enc_hidden = encoder.initialize_hidden_state()
+  total_loss = 0
+
+  for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
+    batch_loss = train_step(inp, targ, enc_hidden)
+    total_loss += batch_loss
+
+    if batch % 100 == 0:
+        print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                     batch,
+                                                     batch_loss.numpy()))
+  # 2 エポックごとにモデル（のチェックポイント）を保存
+  if (epoch + 1) % 2 == 0:
+    checkpoint.save(file_prefix = checkpoint_prefix)
+
+  print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                      total_loss / steps_per_epoch))
+  print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+def evaluate(sentence):
+    attention_plot = np.zeros((max_length_targ, max_length_inp))
+
+    sentence = preprocess_sentence(sentence)
+
+    inputs = [inp_lang.word_index[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs],
+                                                           maxlen=max_length_inp,
+                                                           padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+
+    result = ''
+
+    hidden = [tf.zeros((1, units))]
+    enc_out, enc_hidden = encoder(inputs, hidden)
+
+    dec_hidden = enc_hidden
+    dec_input = tf.expand_dims([targ_lang.word_index['<start>']], 0)
+
+    for t in range(max_length_targ):
+        predictions, dec_hidden, attention_weights = decoder(dec_input,
+                                                             dec_hidden,
+                                                             enc_out)
+
+        # 後ほどプロットするためにアテンションの重みを保存
+        attention_weights = tf.reshape(attention_weights, (-1, ))
+        attention_plot[t] = attention_weights.numpy()
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+
+        result += targ_lang.index_word[predicted_id] + ' '
+
+        if targ_lang.index_word[predicted_id] == '<end>':
+            return result, sentence, attention_plot
+
+        # 予測された ID がモデルに戻される
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence, attention_plot
+
+# アテンションの重みをプロットする関数
+def plot_attention(attention, sentence, predicted_sentence):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(attention, cmap='viridis')
+
+    fontdict = {'fontsize': 14}
+
+    ax.set_xticklabels([''] + sentence, fontdict=fontdiclt, rotation=90)
+    ax.set_yticklabels([''] + predicted_sentence, fontdict=fontdict)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+def translate(sentence):
+    result, sentence, attention_plot = evaluate(sentence)
+
+    print('Input: %s' % (sentence))
+    print('Predicted translation: {}'.format(result))
+
+    attention_plot = attention_plot[:len(result.split(' ')), :len(sentence.split(' '))]
+    plot_attention(attention_plot, sentence.split(' '), result.split(' '))
+
+# checkpoint_dir の中の最後のチェックポイントを復元
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+translate(u'その声に応じて出て来たのは、美しい支那人の女の子です。')
+
+translate(u'婆さんは嘲るやうに、じろりと相手の顔を見ました。')
+
+translate(u'ぢや間違ひのないやうに、')
+
+# 翻訳あやまりの例
+translate(u'亜米利加人が帰つてしまふと、婆さんは次の間の戸口へ行つて、恵蓮。')
